45b06207ab0263322481bf5483f7d56faee75bc3
Jaren Nawy <jarennawy@gmail.com>
2023-09-28 09:26:31 -0400

    fleshed out slug, added background image to header

--- pages/article/[slug].js
+++ pages/article/[slug].js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router"
 import Data from "../../app/components/data"
 import styles from "./article.module.css"
+import "../../app/globals.css"
 
 
 
@@ -8,13 +9,33 @@ export default function Article(){
     const router = useRouter();
     const slug = router.query.slug;
     const articleData = Data.find((val) => val.id == slug)
+    if (!articleData) return null;
     return(
         <main>
-        <h1>Article</h1>
-        <p>{articleData.title}</p>
-        <p>{articleData.blurb}</p>
-        <p>{articleData.date}</p>
-        <div>{articleData.articleText.map((textChunk) => (<p>{textChunk.data}</p>))}</div>
+            <div className={styles.articleHeader} style={ {backgroundImage: `url('${articleData.image.url}')`}}> 
+        
+        <h1>{articleData.title}</h1>
+        <p className={styles.articleHeaderBlurb}>{articleData.blurb}</p>
+        <p>{new Date(articleData.publishedDate).toDateString()}</p>
+        </div>
+        <div className={styles.articleBody}>
+            <div className={styles.articleWrapper}>
+            {articleData.articleText.map((textChunk) => {
+            switch (textChunk.type) {
+                case "p":
+                    return <p>{textChunk.data}</p>
+                case "h2":
+                    return <h2>{textChunk.data}</h2>  
+                case "h3":
+                    return <h3>{textChunk.data}</h3>      
+                default:  
+                return <p>{textChunk.data}</p>
+            }
+        })}
+            
+        
+            </div>
+            </div>
         </main>
     )
 };
